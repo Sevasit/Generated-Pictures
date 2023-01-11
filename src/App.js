5f560b2f52e046ec6a5ef5a5217522844c655070
Sevasit <new12253952@gmail.com>
2023-01-11 19:22:42 +0700

    body has finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-function App() {
+import React, { Suspense } from "react";
+import { Switch, Redirect, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import SectionAi from "./components/SectionAi";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1 className="bg-red-400">fshfshfshfshf</h1>
-    </div>
+    <Suspense>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/home" />
+            </Route>
+            <Route path="/home">
+              <SectionAi />
+            </Route>
+          </Switch>
+        </main>
+        <Footer />
+      </div>
+    </Suspense>
   );
-}
+};
 
 export default App;
--- src/components/SectionAi.jsx
+++ src/components/SectionAi.jsx
@@ -1,13 +1,66 @@
-import React from "react";
-const { Configuration, OpenAIApi } = require("openai");
+import React, { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
 
 const SectionAi = () => {
-  return <div>SectionAi</div>;
+  const [text, setText] = useState("");
+  const [image, setImage] = useState("");
+  const [fetch, setFetch] = useState(false);
+  const [error, setError] = useState(null);
+
+  const generateHandler = async (e) => {
+    e.preventDefault();
+    try {
+      setFetch(true);
+      const response = await openai.createImage({
+        prompt: text.toString(),
+        n: 1,
+        size: "1024x1024",
+      });
+      console.log(response);
+      const image_url = response.data.data[0].url;
+      setImage(image_url);
+      setText("");
+    } catch (error) {
+      setError("Something went wrong!");
+      setTimeout(() => {
+        setError(null);
+      }, 5000);
+    }
+    setFetch(false);
+  };
+
+  const handlerChageText = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Generate Image By OpenAI</h1>
+      <form onSubmit={generateHandler}>
+        <input
+          type="text"
+          value={text}
+          onChange={handlerChageText}
+          placeholder="Text here!"
+          required
+        />
+        <button type="submit">Generate Image</button>
+      </form>
+      {fetch && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      <div>
+        {image && (
+          <img src={image} width="500" height="500" alt="generatePic" />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default SectionAi;
